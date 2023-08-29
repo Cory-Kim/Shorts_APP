606caaed5b56b71a26c0d31bb698bbf64afed6b6
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-08-29 02:39:31 -0700

    set tailwindCSS

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import 'tailwindcss/tailwind.css';
+
+const Home: NextPage = () =>
+{
+  return (
+    <h2 className='text-3xl font-bold underline'>
+      Shorts App
+    </h2>
+  )
+}
+
+export default Home
