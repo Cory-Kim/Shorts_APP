09a9c34fe56e2984ae41c923a952f913495dcbb9
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-08-30 05:27:32 -0700

    working on navbar and sidebar.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google'; // Import the GoogleOAuthProvider
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
+import { ImCancelCircle } from 'react-icons/im';
+import { GoogleLogin } from '@react-oauth/google';
 
 const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+  const userProfile = false;
+
+  const normalLink = 'flex item-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded';
+
   return (
-    <div>Sidebar</div>
-  )
+    <GoogleOAuthProvider clientId=""> {/* Wrap with GoogleOAuthProvider */}
+      <div>
+        <div
+          className='block xl:hidden m-2 ml-4 mt-3 text-xl'
+          onClick={() => setShowSidebar((prev) => !prev)}
+        >
+          {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+        </div>
+
+        {showSidebar && (
+          <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
+            <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
+              {/* ... other code ... */}
+            </div>
+            {!userProfile && (
+              <div className='px-2 py-4 hidden xl:block'>
+                <p className='text-gray-400'>Log in to like and comment on videos</p>
+                <div className='pr-4'>
+                  <GoogleLogin
+                    onSuccess={credentialResponse => {
+                      console.log(credentialResponse);
+                    }}
+                    onError={() => {
+                      console.log('Login Failed');
+                    }}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </GoogleOAuthProvider>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
---
+++ utils/index.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_RUL;
+
+export const fetchGoogleResponse = async (response: any, addUser: any) =>
+{
+  
+};
+
