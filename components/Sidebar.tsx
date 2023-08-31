416b345350a40fd37ca08566e361e3c173e7fe97
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-08-31 02:09:51 -0700

    roughly finished sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,10 @@ import { GoogleOAuthProvider } from '@react-oauth/google'; // Import the GoogleO
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
 import { GoogleLogin } from '@react-oauth/google';
+import Discover from './Discover';
+import SuggestedAccounts from './SuggestedAccounts';
+import Footer from './Footer';
+import Link from 'next/link';
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true);
@@ -23,7 +27,14 @@ const Sidebar = () => {
         {showSidebar && (
           <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
             <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
-              {/* ... other code ... */}
+              <Link href="/">
+                <div className={normalLink}>
+                  <p className='text-2xl'><AiFillHome /></p>
+                  <span className='text-xl hidden xl:block'>
+                    For You
+                  </span>
+                </div>
+              </Link>
             </div>
             {!userProfile && (
               <div className='px-2 py-4 hidden xl:block'>
@@ -37,11 +48,18 @@ const Sidebar = () => {
                       console.log('Login Failed');
                     }}
                   />
+
                 </div>
               </div>
             )}
+
+            <Discover />
+            <SuggestedAccounts />
+            <Footer />
+            
           </div>
         )}
+
       </div>
     </GoogleOAuthProvider>
   );
